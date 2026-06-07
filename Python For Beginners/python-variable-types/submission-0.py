13148d926e46f9856baf29158a434a37b540325e
tinybit-0 <vikramamit100@gmail.com>
2026-06-07 13:13:41 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,10 @@
+integer_type = 100
+float_type = 29.9
+bool_type = 10>2
+string_type = "HelloWorld"
+list_type = [12,3,34,'sfsdf']
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
