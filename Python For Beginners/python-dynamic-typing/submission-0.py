e7651689a3c87216967eb3143551530456a6ad85
tinybit-0 <vikramamit100@gmail.com>
2026-06-07 13:15:41 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 12
+print(type(variable))
+variable = 45.45
+print(type(variable))
+variable = 23<21
+print(type(variable))
+variable = "dfdfdf"
+print(type(variable))
+variable = [34,545,454,45]
+print(type(variable))
