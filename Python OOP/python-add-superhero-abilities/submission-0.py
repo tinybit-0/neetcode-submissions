e97f117b25ab75d2bd30ae24ce5c430d84f614e4
tinybit-0 <vikramamit100@gmail.com>
2026-06-29 22:27:53 +0530

    Add: python-add-superhero-abilities - submission-0

---
+++ Python OOP/python-add-superhero-abilities/submission-0.py
@@ -0,0 +1,31 @@
+class SuperHero:
+    """
+    A class to represent a superhero.
+    
+    Attributes:
+        name (str): The superhero's name
+        power (str): The superhero's main superpower
+        health (int): The superhero's health points
+    """
+    
+    def __init__(self, name: str, power: str, health: int):
+        self.name = name
+        self.power = power
+        self.health = health
+    
+    def attack():
+        print(f"{self.name} attacks with {self.power}!")
+
+    def heal():
+        self.health =+ 10
+        print(f"{self.name} heals 10 points. New health: {self.health}")
+
+    # TODO: Define attack method and implement it
+
+    # TODO: Define heal method and implment it
+     
+
+# TODO: Create superhero instance
+
+
+# TODO: Use the attack() and heal() method
