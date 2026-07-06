995805bfd380806ff15d908773bcced712cebdda
tinybit-0 <vikramamit100@gmail.com>
2026-07-07 00:35:56 +0530

    Modify duplicate check logic in submission-4.py
    Replaced return statement with a variable assignment and return.

--- Data Structures & Algorithms/duplicate-integer/submission-4.py
+++ Data Structures & Algorithms/duplicate-integer/submission-4.py
@@ -4,4 +4,6 @@ class Solution:
         for i in range(len(nums) - 1):
             if nums[i] == nums[i + 1]:
                 return True
-        return False
+        x = False
+        return X
+        
